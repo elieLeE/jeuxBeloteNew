082263e6b0165775521fb26951e26d78220a5b18
LI <duleu.elie@gmail.com>
2025-10-03 23:44:17 +0200

    src: add methods to display a card value.
    Add 2 methods to get string value from couleur_t and rang_t enum.
    Add a FMT and its value to display a card.
    
    It has to be used like this: printf(CARD_FMT, CARD_FMT_ARG(card));

--- src/core/carte.h
+++ src/core/carte.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#include "../../libC/src/logger/logger.h"
+
+#include "../defines.h"
+
 typedef enum rang_t {
     SEPT,
     HUIT,
@@ -26,5 +30,35 @@ typedef struct carte_t {
     rang_t r;
     couleur_t c;
 } carte_t;
+
+inline static const char* name_rang(rang_t r)
+{
+    static const char* name[NBRE_CARTES_BY_COUL] = {
+        "SEPT", "HUIT", "NEUF", "VALET", "DAME", "ROI", "DIX", "AS"};
+
+    if ( r>= SEPT && r <= AS) {
+        return name[r];
+    }
+
+    logger_fatal("rang_t inconnu: %d\n", r);
+    return "";
+}
+
+inline static const char* name_coul(couleur_t c)
+{
+    static const char* name[NBRE_COUL] = {
+        "CARREAU", "COEUR", "PIQUE", "TREFLE"};
+
+    if (c >= CARREAU && c <= TREFLE) {
+        return name[c];
+    }
+
+    logger_fatal("couleur_t unknown: %d\n", c);
+    return "";
+}
+
+#define CARD_FMT "%s de %s"
+#define CARD_FMT_ARG(_c) \
+    name_rang(_c->r), name_coul(_c->c)
 
 #endif
